a80094c6ae1e3fd1f9046cb564e563130347abb6
jps sasadara <jpssasadara1995@gmail.cpm>
2019-08-13 09:39:00 +0530

    create a new bean

---
+++ spring_demo_one/src/com/luv2code/springdemo/CricketCoach.java
@@ -0,0 +1,14 @@
+package com.luv2code.springdemo;
+
+public class CricketCoach implements Coach {
+	public String getCricketCoach() {
+		return "All the team member should play";
+	}
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "All the team member should play";
+	}
+
+}
--- spring_demo_one/src/com/luv2code/springdemo/HelloSpringApp.java
+++ spring_demo_one/src/com/luv2code/springdemo/HelloSpringApp.java
@@ -7,10 +7,16 @@ public class HelloSpringApp {
 	public static void main(String[] args) {
 		//load the spring configuration file
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
 		//retrieve bean from spring container
 		Coach theCoach = context.getBean("myCoach",Coach.class);
 		//call the method on the bean
 		System.out.println(theCoach.getDailyWorkout());
+		
+		//retrieve second bean from spring container
+		Coach theCricketCoach = context.getBean("myCricketCoach",Coach.class);
+		//call the method on the new bean
+		System.out.println(theCricketCoach.getDailyWorkout());
 		//close
 		context.close();
 	}
